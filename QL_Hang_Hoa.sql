7c962b8af42346554e9e513034269a7adcb80320
ducluong16 <luong16112000@gmail.com>
2023-09-14 20:09:23 +0700

    Bổ sung cơ hở dữ liệu hàng hoá và các câu truy vấn

--- QL_Hang_Hoa.sql
+++ QL_Hang_Hoa.sql
@@ -1,36 +1,42 @@
-Create database QL_HangHoa
-Use QL_HangHoa
-CREATE TABLE HOADON(
-   soHD INT PRIMARY KEY,
-   ngaylapHD DATE,
-   maKH INT,
-   FOREIGN KEY (maKH) REFERENCES KHACHHANG(maKH)
-);
+Create database QL_HangHoa1
+Use QL_HangHoa1
 
-CREATE TABLE KHACHHANG(
-   maKH INT PRIMARY KEY,
-   hotenKH NVARCHAR(50),
-   diachi NVARCHAR(100),
-   SDT NVARCHAR(20)
-);
+create table HOADON
+(
+soHD int check(soHD > 1) primary key,
+ngaylapHD date not null,
+maKH char(10) 
+FOREIGN KEY (maKH) REFERENCES KHACHHANG(maKH)
+)
 
-CREATE TABLE HANG(
-   maHang INT PRIMARY KEY,
-   tenHang NVARCHAR(50),
-   DVT NVARCHAR(20),
-   dongia FLOAT,
-   soluong INT
-);
+create table KHACHHANG
+(
+maKH char(10) primary key,
+hotenKH nvarchar(50) not null,
+diachi nvarchar(50),
+SDT char(10)
+)
 
-CREATE TABLE CHITIETHD(
+create table HANG
+(
+maHang char(10) primary key,
+tenHang nvarchar(50) not null,
+DVT nvarchar(50),
+dongia int check(dongia > 1),
+soluong int check(soluong > 1)
+)
+
+create table CHITIETHD
+(
    soHD INT,
-   maHang INT,
+   maHang CHAR(10),
    giaban FLOAT,
    soluong INT,
    PRIMARY KEY (soHD, maHang),
    FOREIGN KEY (soHD) REFERENCES HOADON(soHD),
    FOREIGN KEY (maHang) REFERENCES HANG(maHang)
 );
+
 
 Insert into HOADON(soHD, ngaylapHD, maKH)
 Values 
@@ -85,3 +91,20 @@ Values
 ('H13', N'Truyện tranh', N'quyển', '20000', '100'),
 ('H14', N'Phấn bảng', N'hộp', '30000', '40'),
 ('H15', N'Phấn màu', N'hộp', '40000', '40')
+Insert into CHITIETHD(soHD,maHang,giaban,soluong)
+Values 
+('2','KH1',10,5),
+('3','KH2',15,4),
+('4','KH3',10,3),
+('5','KH4',9,5),
+('6','KH5',11,8),
+('7','KH6',7,6),
+('8','KH7',8,9),
+('9','KH8',10,4),
+('10','KH1',7,6),
+('11','KH10',4,3),
+('12','KH11',15,7),
+('13','KH12',11,10),
+('14','KH13',5,8),
+('15','KH14',6,7), 
+('16','KH15',7,9) 
